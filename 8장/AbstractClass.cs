449b1a3bf7acd2fac56bf3a5e68ae1cdd5a4238e
Hoeya <53281767+Gyudev@users.noreply.github.com>
2020-01-04 14:16:45 +0900

    8장 마무리

---
+++ 8장/AbstractClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AbstractClass
+{
+	public abstract class AbstractBase
+	{
+		protected void PrivateMethodA()
+		{
+			Console.WriteLine("AstractBase.PrivateMethodA()");
+		}
+
+		public void PublicMethodA()
+		{
+			Console.WriteLine("AbstractBase.PublicMethodA()");
+		}
+
+		public abstract void AbstractMethodA();
+	}
+
+	public class Derived : AbstractBase
+	{
+		public override void AbstractMethodA()
+		{
+			Console.WriteLine("Derived.AbstractMethodA()");
+			PrivateMethodA();
+		}
+	}
+
+	class AbstractClass
+	{
+		static void Main(string[] args)
+		{
+			AbstractBase obj = new Derived();
+			obj.AbstractMethodA();
+			obj.PublicMethodA();
+		}
+	}
+}
