21a755313b83da424dc15c6f9bf6d5f03b7a0a60
Hoeya <53281767+Gyudev@users.noreply.github.com>
2020-01-05 13:15:37 +0900

    9장 예제 마무리

---
+++ 9장/AnonymousTypeProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnonymousTypeProperty
+{
+	class AnonymousTypeProperty
+	{
+		static void Main(string[] args)
+		{
+			var a = new { Name = "유병규", Age = "26" };
+			Console.WriteLine($"Name : {a.Name}, Age : {a.Age}");
+
+			var b = new { Subject = "수학", Scores = new int[] { 90, 80, 70, 60 } };
+			Console.Write($"Subject : {b.Subject}, Score : ");
+			foreach(var score in b.Scores)
+			{
+				Console.Write($"{score} ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ 9장/PropertiesInInterface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PropertiesInInterface
+{
+	interface INamedValue
+	{
+		string Name { get; set; }
+		string Value { get; set; }
+	}
+
+	class NamedValue : INamedValue
+	{
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+
+	class PropertiesInInterface
+	{
+		static void Main(string[] args)
+		{
+			NamedValue name = new NamedValue() { Name = "이름", Value = "유병규" };
+			NamedValue height = new NamedValue() { Name = "키", Value = "173cm" };
+			NamedValue weight = new NamedValue() { Name = "몸무게", Value = "60kg" };
+
+			Console.WriteLine($"{name.Name} : {name.Value}");
+			Console.WriteLine($"{height.Name} : {height.Value}");
+			Console.WriteLine($"{weight.Name} : {weight.Value}");
+
+		}
+	}
+}
