78e435ef45d9e17094c8bcef8d3864ab243aea0d
Hoeya <53281767+Gyudev@users.noreply.github.com>
2020-01-06 12:46:41 +0900

    9장 연습문제

---
+++ 9장/연습문제/Ex9_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9_1
+{
+	class NameCrad
+	{
+		public int Age { get; set; }
+		public string Name { get; set; }
+	}
+
+	class Ex9_1
+	{
+		static void Main(string[] args)
+		{
+			NameCrad myCard = new NameCrad();
+			myCard.Age = 26;
+			myCard.Name = "유병규";
+
+			Console.WriteLine($"나이 : {myCard.Age}");
+			Console.WriteLine($"이름 : {myCard.Name}");
+		}
+	}
+}
---
+++ 9장/연습문제/Ex9_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9_2
+{
+	class Ex9_2
+	{
+		static void Main(string[] args)
+		{
+			var nameCard = new { Name = "유병규", Age = 26 };
+			Console.WriteLine($"이름 : {nameCard.Name}, 나이 : {nameCard.Age}");
+
+			var complex = new { Real = 3, Imaginary = -12 };
+			Console.WriteLine($"Real : {complex.Real}, Imaginary : {complex.Imaginary}");
+		}
+	}
+}
